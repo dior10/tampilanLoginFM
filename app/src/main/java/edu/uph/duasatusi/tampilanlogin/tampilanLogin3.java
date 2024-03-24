3e4722c0c742f2e9fb4a8610b93b846ce5ca48b5
Nicholas Calim <nicholas.uph@gmail.com>
2024-03-24 21:26:19 +0700

    tampilan login 1,2,3

---
+++ app/src/main/java/edu/uph/duasatusi/tampilanlogin/tampilanLogin3.java
@@ -0,0 +1,14 @@
+package edu.uph.duasatusi.tampilanlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tampilanLogin3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_login3);
+    }
+}
